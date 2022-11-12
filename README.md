b0be169af99bfa8385be33b4088a2e8c2beba6ea
Arlo O'Keeffe <tiger@k5d.de>
2022-11-12 10:35:56 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,3 @@
+# postgres-sync-demo
+
+A demo on how use triggers, queues, etc. to sync the app's data somewhere else.
---
+++ src/main/java/io/github/arlol/postgressyncdemo/PostgresSyncDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.arlol.postgressyncdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresSyncDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresSyncDemoApplication.class, args);
+	}
+
+}
