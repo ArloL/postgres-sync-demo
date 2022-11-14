e34de2138e3cbef611a8413b54d65cd2eee88a11
Arlo O'Keeffe <tiger@k5d.de>
2022-11-14 08:27:51 +0100

    explicitly import DataSourceAutoConfiguration

--- src/main/java/io/github/arlol/postgressyncdemo/PostgresSyncDemoApplication.java
+++ src/main/java/io/github/arlol/postgressyncdemo/PostgresSyncDemoApplication.java
@@ -2,10 +2,13 @@ package io.github.arlol.postgressyncdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@Import(DataSourceAutoConfiguration.class)
 public class PostgresSyncDemoApplication {
 
 	public static void main(String[] args) {
