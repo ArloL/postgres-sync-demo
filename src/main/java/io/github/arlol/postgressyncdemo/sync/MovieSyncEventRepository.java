2c7e93f054ff299c884662fb87e5d1eade195469
Arlo O'Keeffe <tiger@k5d.de>
2022-11-17 20:53:40 +0100

    make MovieSyncEventRepository a PagingAndSortingRepository

--- src/main/java/io/github/arlol/postgressyncdemo/sync/MovieSyncEventRepository.java
+++ src/main/java/io/github/arlol/postgressyncdemo/sync/MovieSyncEventRepository.java
@@ -3,12 +3,12 @@ package io.github.arlol.postgressyncdemo.sync;
 import java.util.Optional;
 
 import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieSyncEventRepository
-		extends CrudRepository<MovieSyncEvent, Long> {
+		extends PagingAndSortingRepository<MovieSyncEvent, Long> {
 
 	@Query(
 		"""
