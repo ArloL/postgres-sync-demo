69d8b62afe5df43dbe76dbe268a87830e5a5c7e6
Arlo O'Keeffe <tiger@k5d.de>
2022-11-17 20:38:28 +0100

    add singleThreadExecutor

--- src/main/java/io/github/arlol/postgressyncdemo/PostgresSyncDemoApplication.java
+++ src/main/java/io/github/arlol/postgressyncdemo/PostgresSyncDemoApplication.java
@@ -1,8 +1,14 @@
 package io.github.arlol.postgressyncdemo;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -16,6 +22,17 @@ public class PostgresSyncDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PostgresSyncDemoApplication.class, args);
+	}
+
+	@Bean
+	public Executor singleThreadExecutor() {
+		return new ThreadPoolExecutor(
+				1,
+				1,
+				0L,
+				TimeUnit.MILLISECONDS,
+				new LinkedBlockingDeque<>(1)
+		);
 	}
 
 }
