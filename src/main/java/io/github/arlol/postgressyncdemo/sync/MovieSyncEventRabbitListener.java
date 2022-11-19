46c9bc94ff7b3d8d61906598869e63d04b523157
Arlo O'Keeffe <tiger@k5d.de>
2022-11-19 23:12:53 +0100

    add MovieSyncEventRabbitListener

---
+++ src/main/java/io/github/arlol/postgressyncdemo/sync/MovieSyncEventRabbitListener.java
@@ -0,0 +1,22 @@
+package io.github.arlol.postgressyncdemo.sync;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieSyncEventRabbitListener {
+
+	private final MovieSyncEventToDatabase processor;
+
+	public MovieSyncEventRabbitListener(
+			MovieSyncEventToDatabase processor
+	) {
+		this.processor = processor;
+	}
+
+	@RabbitListener(queues = "#{syncEventQueue}")
+	public void receiveMessageFromFanout(MovieSyncEvent event) {
+		processor.accept(event);
+	}
+
+}
