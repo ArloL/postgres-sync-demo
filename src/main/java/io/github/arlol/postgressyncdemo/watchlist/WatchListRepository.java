141e0c44bfabe55f5a1a8471832cedbda4922107
Arlo O'Keeffe <tiger@k5d.de>
2022-11-19 23:04:11 +0100

    make WatchListRepository a PagingAndSortingRepository

--- src/main/java/io/github/arlol/postgressyncdemo/watchlist/WatchListRepository.java
+++ src/main/java/io/github/arlol/postgressyncdemo/watchlist/WatchListRepository.java
@@ -2,11 +2,12 @@ package io.github.arlol.postgressyncdemo.watchlist;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WatchListRepository extends CrudRepository<WatchList, Long> {
+public interface WatchListRepository
+		extends PagingAndSortingRepository<WatchList, Long> {
 
 	void deleteByMovieId(Long movieId);
 
