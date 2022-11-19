98304a7de03a553fb518731519963f70e6bf3abd
Arlo O'Keeffe <tiger@k5d.de>
2022-11-19 23:09:15 +0100

    use repositories from superclass

--- src/test/java/io/github/arlol/postgressyncdemo/sync/MovieSyncEventRepositoryTest.java
+++ src/test/java/io/github/arlol/postgressyncdemo/sync/MovieSyncEventRepositoryTest.java
@@ -4,38 +4,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import io.github.arlol.postgressyncdemo.DatabaseTest;
 import io.github.arlol.postgressyncdemo.movie.Movie;
-import io.github.arlol.postgressyncdemo.movie.MovieRepository;
 
 @SpringBootTest
 public class MovieSyncEventRepositoryTest extends DatabaseTest {
 
-	@Autowired
-	MovieSyncEventRepository repository;
-	@Autowired
-	MovieRepository movieRepository;
-
 	@Test
 	void testFindAll() throws Exception {
-		assertThat(repository.findAll()).isEmpty();
+		assertThat(movieSyncEventRepository.findAll()).isEmpty();
 	}
 
 	@Test
 	void testSave() throws Exception {
 		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
-			repository.save(MovieSyncEvent.builder().action("I").build());
+			movieSyncEventRepository
+					.save(MovieSyncEvent.builder().action("I").build());
 		});
 	}
 
 	@Test
 	void testFindAndDeleteNextSyncEvent() throws Exception {
-		assertThat(repository.findAll()).isEmpty();
+		assertThat(movieSyncEventRepository.findAll()).isEmpty();
 
-		var nextSyncEvent = repository.findAndDeleteNextSyncEvent();
+		var nextSyncEvent = movieSyncEventRepository
+				.findAndDeleteNextSyncEvent();
 		assertThat(nextSyncEvent).isEmpty();
 
 		Movie movie = Movie.builder().title("MSERT Batman").build();
@@ -44,24 +39,24 @@ public class MovieSyncEventRepositoryTest extends DatabaseTest {
 				.save(movie.toBuilder().title("MSERT Batman Begins").build());
 		movieRepository.delete(movie);
 
-		assertThat(repository.findAll()).isNotEmpty();
+		assertThat(movieSyncEventRepository.findAll()).isNotEmpty();
 
-		nextSyncEvent = repository.findAndDeleteNextSyncEvent();
+		nextSyncEvent = movieSyncEventRepository.findAndDeleteNextSyncEvent();
 		assertThat(nextSyncEvent).isNotEmpty();
 		assertThat(nextSyncEvent.get().getAction()).isEqualTo("I");
 		assertThat(nextSyncEvent.get().getMovieId()).isEqualTo(movie.getId());
 
-		nextSyncEvent = repository.findAndDeleteNextSyncEvent();
+		nextSyncEvent = movieSyncEventRepository.findAndDeleteNextSyncEvent();
 		assertThat(nextSyncEvent).isNotEmpty();
 		assertThat(nextSyncEvent.get().getAction()).isEqualTo("U");
 		assertThat(nextSyncEvent.get().getMovieId()).isEqualTo(movie.getId());
 
-		nextSyncEvent = repository.findAndDeleteNextSyncEvent();
+		nextSyncEvent = movieSyncEventRepository.findAndDeleteNextSyncEvent();
 		assertThat(nextSyncEvent).isNotEmpty();
 		assertThat(nextSyncEvent.get().getAction()).isEqualTo("D");
 		assertThat(nextSyncEvent.get().getMovieId()).isEqualTo(movie.getId());
 
-		assertThat(repository.findAll()).isEmpty();
+		assertThat(movieSyncEventRepository.findAll()).isEmpty();
 	}
 
 }
