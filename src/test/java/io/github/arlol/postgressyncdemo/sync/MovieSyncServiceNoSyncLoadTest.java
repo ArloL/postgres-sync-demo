253ca3272c3cae346a2933b1411418196c84c138
Arlo O'Keeffe <tiger@k5d.de>
2022-11-20 14:02:06 +0100

    add load test without sync

---
+++ src/test/java/io/github/arlol/postgressyncdemo/sync/MovieSyncServiceNoSyncLoadTest.java
@@ -0,0 +1,41 @@
+package io.github.arlol.postgressyncdemo.sync;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import io.github.arlol.postgressyncdemo.DatabaseTest;
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootTest
+@Slf4j
+@Tag("load")
+public class MovieSyncServiceNoSyncLoadTest extends DatabaseTest {
+
+	@Autowired
+	MovieSyncEventDatabaseListener listener;
+	@Autowired
+	MovieSyncService movieSyncService;
+
+	LoadTest loadTest;
+
+	@BeforeEach
+	public void beforeEach() throws Exception {
+		loadTest = new LoadTest(
+				log,
+				listener,
+				movieSyncService,
+				movieRepository,
+				movieSyncEventRepository,
+				watchListRepository
+		);
+	}
+
+	@Test
+	void test() throws Exception {
+		loadTest.test(0);
+	}
+
+}
