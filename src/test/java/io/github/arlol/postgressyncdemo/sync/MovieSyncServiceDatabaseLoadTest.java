6e8d5c9d8b0448a26bb0264096982d201bcb02b0
Arlo O'Keeffe <tiger@k5d.de>
2022-11-20 13:58:51 +0100

    add load tag

--- src/test/java/io/github/arlol/postgressyncdemo/sync/MovieSyncServiceDatabaseLoadTest.java
+++ src/test/java/io/github/arlol/postgressyncdemo/sync/MovieSyncServiceDatabaseLoadTest.java
@@ -2,6 +2,7 @@ package io.github.arlol.postgressyncdemo.sync;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest
 @Slf4j
+@Tag("load")
 public class MovieSyncServiceDatabaseLoadTest extends DatabaseTest {
 
 	@Autowired
