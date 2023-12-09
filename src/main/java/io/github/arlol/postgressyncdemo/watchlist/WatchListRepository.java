12e41bd4e4456c4de1cfcdc704b8d2897b618503
Arlo O'Keeffe <tiger@k5d.de>
2023-12-09 20:19:48 +0100

    make WatchListRepository immutable

--- src/main/java/io/github/arlol/postgressyncdemo/watchlist/WatchListRepository.java
+++ src/main/java/io/github/arlol/postgressyncdemo/watchlist/WatchListRepository.java
@@ -2,10 +2,12 @@ package io.github.arlol.postgressyncdemo.watchlist;
 
 import java.util.Optional;
 
+import org.springframework.data.annotation.Immutable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Immutable
 public interface WatchListRepository extends CrudRepository<WatchList, Long> {
 
 	void deleteByMovieId(Long movieId);
